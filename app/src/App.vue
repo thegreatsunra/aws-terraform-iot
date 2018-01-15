04601c997ab553a84632a7709027b675f20a7661
Dane Petersen <thegreatsunra@gmail.com>
2018-01-15 14:50:49 -0600

    Add package.json, app dependencies, README, entry files

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted () {
+    setInterval(() => this.$store.dispatch('pullDevices'), 5000)
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles/app';
+</style>
---
+++ app/src/main.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0)
+  next()
+})
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
