88f46dac45320b6adaee72daa2091afea55b807f
Dane Petersen <thegreatsunra@gmail.com>
2018-01-15 14:37:09 -0600

    Stub out gateway app, which will emit fake IoT device events

---
+++ gateway/.eslintrc.js
@@ -0,0 +1,16 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  plugins: [],
+  'rules': {}
+}
