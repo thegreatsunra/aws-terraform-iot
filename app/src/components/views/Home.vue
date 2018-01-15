43757f8304d168037061b932ebc9f8a9464d126c
Dane Petersen <thegreatsunra@gmail.com>
2018-01-15 14:57:19 -0600

    Add default home view

---
+++ app/src/components/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="view _view view--padded">
+    <button class="_btn btn" @click="logOut()">Sign out</button>
+    <h1>Home</h1>
+    <h3><router-link to="/devices">Devices</router-link></h3>
+    <h3><router-link to="/events">Events</router-link></h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    this.$store.dispatch('pullDevices')
+  },
+  methods: {
+    logOut () {
+      this.$router.push('/logout')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
