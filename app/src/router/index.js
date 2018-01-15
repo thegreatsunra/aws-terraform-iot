af119d5056890eb5117845a6504da1ffae7ee7ff
Dane Petersen <thegreatsunra@gmail.com>
2018-01-15 14:54:22 -0600

    Add cognito auth to router

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,16 +1,36 @@
 import Router from 'vue-router'
 import Vue from 'vue'
 
+import cognitoAuth from '@/cognito'
+
 import Home from '@/components/views/Home'
 Vue.use(Router)
+
+function requireAuth (to, from, next) {
+  cognitoAuth.isAuthenticated((err, loggedIn) => {
+    if (err) return next()
+    if (!loggedIn) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  })
+}
+
 export default new Router({
   routes: [
     {
       path: '/home',
       component: Home,
+      beforeEnter: requireAuth
+    },
     },
     {
       path: '*',
       redirect: '/home'
     }
   ]
+})
