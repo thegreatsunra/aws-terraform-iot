26d653157f0b8867db0163ad213b714d2839e4e9
Dane Petersen <thegreatsunra@gmail.com>
2018-01-15 14:43:31 -0600

    Add placeholder config file and empty cert folder for AWS IoT certs

---
+++ gateway/config.example.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const config = {
+  aws: {
+    iot: {
+      keyPath: path.join(__dirname, './cert/private.pem.key'),
+      certPath: path.join(__dirname, './cert/certificate.pem.crt'),
+      caPath: path.join(__dirname, './cert/root-CA.pem'),
+      host: 'xxxxxxxxxxxxx.iot.xx-xxxx-x.amazonaws.com',
+      topic: 'xxxxxxxxxxxxxx'
+    }
+  }
+}
+
+module.exports = config
