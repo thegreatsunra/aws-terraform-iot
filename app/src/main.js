bd5699d71e6c6f1169593d5d0fedf07d07e9d928
Dane Petersen <thegreatsunra@gmail.com>
2018-12-26 12:12:28 -0600

    Replace main.js with vue-cli boilerplate

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import App from './App'
-import axios from 'axios'
-import cognitoAuth from './cognito'
 import Vue from 'vue'
-
+import App from './App.vue'
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
+import axios from 'axios'
+import cognitoAuth from './cognito'
 
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
@@ -16,12 +14,9 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  cognitoAuth,
   router,
   store,
-  template: '<App/>'
-})
+  cognitoAuth,
+  render: h => h(App)
+}).$mount('#app')
